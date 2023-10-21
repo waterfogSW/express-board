bc4f5bcd2073db8422dda22a28c36c9b4f196df6
San Kim <waterfog9580@gmail.com>
2023-10-21 21:38:25 +0900

    :adhesive_bandage: Refactor import statement in user.controller.ts
    Adjusted the import statement for Controller decorator from a default import to a named import in the user.controller.ts file. This change follows our updated coding standards for improved readability and consistency throughout the codebase.

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import { HttpStatus } from '../common/constant/http.status';
 import { Injectable } from '../common/decorator/injectable.decorator';
 import { UserService } from '../service/user.service';
-import Controller from '../common/decorator/controller.decorator';
+import { Controller } from '../common/decorator/controller.decorator';
 import { Get } from '../common/decorator/handler.decorator';
 
 @Injectable()
